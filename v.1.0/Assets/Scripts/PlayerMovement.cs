3dda7da03b30053da2df8d5d41080bb8a21e8451
Cristian David Campos Duran <134449312+Cristian-Universidad@users.noreply.github.com>
2024-05-01 13:24:13 -0500

    Character Animation Commit
    En este commit se han añadido las animaciones de los personajes, Luigi y Mario, así como también los sonidos correspondientes a sus saltos.

--- v.1.0/Assets/Scripts/PlayerMovement.cs
+++ v.1.0/Assets/Scripts/PlayerMovement.cs
@@ -9,18 +9,27 @@ public class PlayerMovement : MonoBehaviour
     public float JumpForce;
 
     private Rigidbody2D Rigidbody2D;
+    private Animator Animator; /*Animacion Jugador*/
     private float Horizontal;
     private bool Grounded;
+
+    [SerializeField] private AudioClip saltoSonido;
 
     void Start()
     {
         Rigidbody2D = GetComponent<Rigidbody2D>();
+        Animator = GetComponent<Animator>();/*Animacion*/
     }
 
     // Update is called once per frame
     void Update()
     {
         Horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Horizontal < 0.0f) transform.localScale = new Vector3(-0.4898234f, 0.4898234f, 0.4898234f);
+        else if (Horizontal > 0.0f) transform.localScale = new Vector3(0.4898234f, 0.4898234f, 0.4898234f);
+
+       Animator.SetBool("running", Horizontal != 0.0f);  /*Animacion*/
 
         if (Physics2D.Raycast(transform.position, Vector3.down, 0.1f))
         {
@@ -32,6 +41,7 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.W) && Grounded)
         {
             Jump();
+            ControladorSonido.Instance.EjecutarSonido(saltoSonido);
         }
 
     }
---
+++ v.1.0/Assets/Scripts/PlayerMovementLuigi.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementLuigi : MonoBehaviour
+{
+
+    public float Speed;
+    public float JumpForce;
+
+    private Rigidbody2D Rigidbody2D;
+    private Animator Animator; /*Animacion Jugador*/
+    private float Horizontal;
+    private bool Grounded;
+
+    [SerializeField] private AudioClip saltoSonido;
+
+    void Start()
+    {
+        Rigidbody2D = GetComponent<Rigidbody2D>();
+        Animator = GetComponent<Animator>();/*Animacion*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Horizontal < 0.0f) transform.localScale = new Vector3(-0.5565351f, 0.5565351f, 0.5565351f);
+        else if (Horizontal > 0.0f) transform.localScale = new Vector3(0.5565351f, 0.5565351f, 0.5565351f);
+
+        Animator.SetBool("running", Horizontal != 0.0f);  /*Animacion*/
+
+        if (Physics2D.Raycast(transform.position, Vector3.down, 0.1f))
+        {
+            Grounded = true;
+        }
+        else Grounded = false;
+
+        // Salto
+        if (Input.GetKeyDown(KeyCode.W) && Grounded)
+        {
+            Jump();
+            ControladorSonido.Instance.EjecutarSonido(saltoSonido);
+        }
+
+    }
+    private void Jump()
+    {
+        Rigidbody2D.AddForce(Vector2.up * JumpForce);
+    }
+    private void FixedUpdate()
+    {
+        Rigidbody2D.velocity = new Vector2(Horizontal * Speed, Rigidbody2D.velocity.y);
+    }
+
+}
