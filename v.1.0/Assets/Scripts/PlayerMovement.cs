35617b89b496a4a6cfd9b1e4a708c1db13467780
JuanTrillos <133701546+JuanTrillos@users.noreply.github.com>
2024-05-06 16:47:40 -0500

    PlayerMovement Code Commit
    Se hizo los scripts de los personajes

--- v.1.0/Assets/Scripts/PlayerMovement.cs
+++ v.1.0/Assets/Scripts/PlayerMovement.cs
@@ -4,12 +4,11 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
     public float Speed;
     public float JumpForce;
 
     private Rigidbody2D Rigidbody2D;
-    private Animator Animator; /*Animacion Jugador*/
+    private Animator Animator;
     private float Horizontal;
     private bool Grounded;
 
@@ -18,24 +17,29 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         Rigidbody2D = GetComponent<Rigidbody2D>();
-        Animator = GetComponent<Animator>();/*Animacion*/
+        Animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         Horizontal = Input.GetAxisRaw("Horizontal");
 
-        if (Horizontal < 0.0f) transform.localScale = new Vector3(-0.4898234f, 0.4898234f, 0.4898234f);
-        else if (Horizontal > 0.0f) transform.localScale = new Vector3(0.4898234f, 0.4898234f, 0.4898234f);
+        if (Horizontal < 0.0f)
+            transform.localScale = new Vector3(-0.5565351f, 0.5565351f, 0.5565351f);
+        else if (Horizontal > 0.0f)
+            transform.localScale = new Vector3(0.5565351f, 0.5565351f, 0.5565351f);
 
-       Animator.SetBool("running", Horizontal != 0.0f);  /*Animacion*/
+        Animator.SetBool("running", Horizontal != 0.0f);
 
-        if (Physics2D.Raycast(transform.position, Vector3.down, 0.1f))
+        // Verificar si el personaje está en el suelo y no está ascendiendo ni descendiendo
+        if (Physics2D.Raycast(transform.position, Vector3.down, 0.1f) && Mathf.Abs(Rigidbody2D.velocity.y) < 0.1f)
         {
             Grounded = true;
         }
-        else Grounded = false;
+        else
+        {
+            Grounded = false;
+        }
 
         // Salto
         if (Input.GetKeyDown(KeyCode.W) && Grounded)
@@ -43,15 +47,16 @@ public class PlayerMovement : MonoBehaviour
             Jump();
             ControladorSonido.Instance.EjecutarSonido(saltoSonido);
         }
-
     }
+
     private void Jump()
     {
         Rigidbody2D.AddForce(Vector2.up * JumpForce);
     }
+
     private void FixedUpdate()
     {
-        Rigidbody2D.velocity = new Vector2(Horizontal*Speed, Rigidbody2D.velocity.y);
+        Rigidbody2D.velocity = new Vector2(Horizontal * Speed, Rigidbody2D.velocity.y);
     }
-
 }
+
