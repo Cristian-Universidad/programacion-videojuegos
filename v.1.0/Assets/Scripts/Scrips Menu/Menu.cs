bc9576bd169c1b8bc92c96dcb50ac194b3bc930f
stiven9010 <165229837+stiven9010@users.noreply.github.com>
2024-05-14 19:23:24 -0500

    Menu inicio
    Configure el inicio del juego

---
+++ v.1.0/Assets/Scripts/Scrips Menu/Menu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+   public void Jugar (string NombreNivel)
+    {
+        SceneManager.LoadScene(NombreNivel);
+    }
+   public void Salir ()
+    {
+        Application.Quit();
+        Debug.Log("Aqui se cierra el juego");
+
+    }
+}
